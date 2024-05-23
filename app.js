88013929ead7157ea6eb3116fc5595e578215ce6
Leonardo Loureiro de Almeida <leolalmeida44@gmail.com>
2024-05-23 20:14:57 -0300

    feat: adição do middleware no app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const rotas = require('./routes/rotas'); // Importa as rotas definidas em 'routes/index.js'
 const app = express();
 const path = require('path'); // Importa o módulo 'path' para trabalhar com diretórios e caminhos de arquivos
+const notFoundMiddleware = require('./middlewares/notFound'); // Importa o middleware de tratamento de erro 404
+
 
 // Configurações de view engine e múltiplas pastas de views
 app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'templates')]); // Define os diretórios de views
@@ -10,6 +12,9 @@ app.engine('html', require('ejs').renderFile); // Configura a view engine para r
 
 // Middleware para usar as rotas definidas
 app.use('/', rotas);
+
+// Middleware para tratar erro 404
+app.use(notFoundMiddleware);
 
 const PORT = 3000;
 
