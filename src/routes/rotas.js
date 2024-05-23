000523533f67acf9d73a041365634f2b75a2ec18
Layon José <layonjp300@gmail.com>
2024-05-23 15:13:25 -0300

    feat: adicionando rota para buscar uma raça de cachorro

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router(); // Cria um roteador para gerenciar as rotas
 
-// Define a rota para a página inicial e associa ao método 'renderHomePage' do 'homeController'
+// Define a rota para a página inicial'
 router.get("/", homeController.renderHomePage);
+
+// Define a rota para buscar uma raça de cachorro'
+router.get("/dog", dogController.getDogByBreed);
 
 module.exports = router; // Exporta o roteador para ser usado em 'app.js'
