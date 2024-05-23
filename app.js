195d6198c9004fce127e6f7283f3cdb9fde8f0b0
Layon José <layonjp300@gmail.com>
2024-05-23 20:04:40 -0300

    feat: configuração de view engine e múltiplas pastas de views

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const rotas = require('./routes/rotas'); // Importa as rotas definidas em 'route
 const app = express();
 const path = require('path'); // Importa o módulo 'path' para trabalhar com diretórios e caminhos de arquivos
 
+// Configurações de view engine e múltiplas pastas de views
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'templates')]); // Define os diretórios de views
+app.set('view engine', 'ejs'); // Define 'ejs' como a view engine
+app.engine('html', require('ejs').renderFile); // Configura a view engine para renderizar arquivos HTML
+
 // Middleware para usar as rotas definidas
 app.use('/', rotas);
 
