c969842ba5d0d249ec455fcbde7b4055135e069d
katfr <katarinalydia@hotmail.com>
2024-05-24 15:24:16 -0300

    fix: corrigindo erro na pagina inicial

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,7 +7,7 @@ exports.renderHomePage = async (req, res) => {
         const response = await axios.get('https://dog.ceo/api/breeds/list/all');
         const breeds = Object.keys(response.data.message); // Extrai as raças da resposta da API
         // Renderiza a página inicial ('index') passando as raças obtidas
-        res.render("index", { dog: null, error: null, breeds, breed});
+        res.render("index", { dog: null, error: null, breeds, breed:''});
     } catch (error) {
         // Em caso de erro, renderiza a página inicial com uma mensagem de erro e uma lista de raças vazia
         res.render("index", { dog: null, error: 'Erro ao buscar lista de raças', breeds: [], breed: ''});
