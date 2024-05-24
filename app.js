c05782cc83dd85f44f6d88d192d254486e97ed10
Leonardo Loureiro de Almeida <leolalmeida44@gmail.com>
2024-05-23 21:37:22 -0300

    fix: correção de diretorio

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const rotas = require('./routes/rotas'); // Importa as rotas definidas em 'routes/index.js'
+const rotas = require('./src/routes/rotas'); // Importa as rotas definidas em 'routes/index.js'
 const app = express();
 const path = require('path'); // Importa o módulo 'path' para trabalhar com diretórios e caminhos de arquivos
-const notFoundMiddleware = require('./middlewares/notFound'); // Importa o middleware de tratamento de erro 404
+const notFoundMiddleware = require('./src/middlewares/notFound'); // Importa o middleware de tratamento de erro 404
 
 
 // Configurações de view engine e múltiplas pastas de views
-app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'templates')]); // Define os diretórios de views
+app.set('views', [path.join(__dirname, 'src','views'), path.join(__dirname, 'src','templates')]); // Define os diretórios de views
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 app.set('view engine', 'ejs'); // Define 'ejs' como a view engine
 app.engine('html', require('ejs').renderFile); // Configura a view engine para renderizar arquivos HTML
 
